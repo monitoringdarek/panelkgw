75db45e97df6741d2d8090c32f003fc5a06add01
monitoringdarek <monitoringdarek@gmail.com>
2026-06-04 00:28:25 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.KGW_CONFIG = {
   SUPABASE_URL: "https://jxmnlscbmmcfjqtnjwcc.supabase.co",
   SUPABASE_KEY: "sb_publishable_WcZPM_0gAwE6HNO5-PGL0A_OgmO6h5-",
-  APP_VERSION: "v2026.06.03-21"
+  APP_VERSION: "v2026.06.03-21b"
 };
